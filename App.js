22231ebc46764372345f5cdc39c414bcf42a96d0
cosmos <alfredchrisayo@gmail.com>
2024-09-18 15:38:05 +0000

    save

--- App.js
+++ App.js
@@ -19,6 +19,8 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+
+
 // https://invoicelogger.netlify.app/
 app.use("/api", routes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
