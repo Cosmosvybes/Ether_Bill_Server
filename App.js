0eeab6d60acbc6b309f9da388fadd61e5937e8ab
cosmos <alfredchrisayo@gmail.com>
2024-09-23 19:20:18 +0000

    add invoice -in-house sending

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ app.use(json());
 // app use cors
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://invoicelogger.netlify.app",
     credentials: true,
     optionsSuccessStatus: 200,
   })
