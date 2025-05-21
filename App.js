37d4657e7b1d0bb754b1480c6d82116b60c8ee6f
cosmos <alfredchrisayo@gmail.com>
2025-05-21 10:07:41 +0000

    production FE url added

--- App.js
+++ App.js
@@ -15,14 +15,12 @@ app.use(json());
 // app use cors
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://invoicelogger.netlify.app",
     credentials: true,
     optionsSuccessStatus: 200,
   })
 );
 // "https://invoicelogger.netlify.app"
-
-
 
 app.use("/api", routes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
