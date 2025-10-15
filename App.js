e5ecbcf48ca7385071cfd0657f895fb826edcac3
cosmosayo <alfredchrisayo@gmail.com>
2025-10-15 06:12:14 +0100

    test mode

--- App.js
+++ App.js
@@ -2,20 +2,18 @@ const express = require("express");
 const { json } = require("express");
 const PORT = process.env.PORT || 8080;
 const { urlencoded } = require("body-parser");
-const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { routes } = require("./API/routes/routes");
 
 const app = express();
 
 app.use(urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(json());
 
 // app use cors
 app.use(
   cors({
-    origin: "https://invoicelogger.netlify.app",
+    origin: "http://localhost:5173",
     credentials: true,
     optionsSuccessStatus: 200,
   })
