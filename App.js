41b19289fc4a9e00a364ecb1d47dc16020e704b0
cosmosayo <alfredchrisayo@gmail.com>
2025-12-05 13:31:58 +0100

    testing

--- App.js
+++ App.js
@@ -7,22 +7,19 @@ const app = express();
 const { requestLogger } = require("./_helper/cluster");
 const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: false }));
-app.use(json());
-
 app.use(
   cors({
     origin: "https://invoicelogger.netlify.app", // prod: "https://invoicelogger.netlify.app"
-    credentials: true,
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.use(json());
+
 app.use(requestLogger());
 app.use("/api", routes);
 
-// serverClusterer();
-
 app.listen(PORT, () =>
-  console.log(`Server instantiated on Worker  ${process.pid}`)
+  console.log(`Server instantiated on Worker ${PORT} on cpu ${process.pid}`)
 );
